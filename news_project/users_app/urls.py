0ce2425f182ba8fecdfdc6a406164a809909588e
aya <eng.aya136@gmail.com>
2016-02-28 20:18:41 +0200

    editing pagination , adding categories

--- news_project/users_app/urls.py
+++ news_project/users_app/urls.py
@@ -2,20 +2,8 @@ from django.conf.urls import url
 from . import views
 
 
-#urlpatterns = [url(r'^$', views.index, name='index'),
-#url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-#url('^list$', views.list, name='list'),
-#url(r'^new$', views.Question_new,name='Questions'),
-#url(r'^c$', views.choice_new, name='choices'),
-#url(r'^clist$', views.choice_list, name='choice_list'),
-
-#url(r'^edit/(?P<question_id>[0-9]+)/$', views.edit_Question,name='Question_edit'),
-#url(r'^editc/(?P<question_id>[0-9]+)/$', views.edit_choice,name='choice_edit'),
-#url(r'^delete/(?P<question_id>[0-9]+)/$', views.delete),
-
-#]
-
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-]
+    url(r'^cat$',views.catt,name='catt'),
+    url(r'^(?P<categoryn>\d+)/index', views.postcat),
 
+]
